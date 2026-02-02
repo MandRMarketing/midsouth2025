c680a0facdb0cd914fdfe32fc76aeba4a9cba7cd
Logan Scheid <scheid.logan@gmail.com>
2026-02-02 16:22:04 -0500

    Refactor featured post module to enhance layout and responsiveness. Update HTML structure for improved clarity and accessibility, including a new grid layout for featured posts. Introduce SCSS styles for better visual consistency and fallback handling, ensuring a more engaging user experience.

--- wp-content/themes/mrmastertheme/views/conditional/posts/archive/modules/featured-post/featured-post.php
+++ wp-content/themes/mrmastertheme/views/conditional/posts/archive/modules/featured-post/featured-post.php
@@ -14,80 +14,55 @@ if (
 ?>
     <section class="featured-post">
         <div class="container">
-            <h4> <?= esc_html($section_title) ?></h4>
-            <?php foreach ($featured_posts as $featured_post) :
-                $post_id = is_object($featured_post) ? $featured_post->ID : (int) $featured_post;
-                $post_image_id = get_post_thumbnail_id($post_id);
+            <h4><?= esc_html($section_title) ?></h4>
+            <div class="featured-post-grid">
+                <?php foreach ($featured_posts as $featured_post) :
+                    $post_id = is_object($featured_post) ? $featured_post->ID : (int) $featured_post;
+                    $post_image_id = get_post_thumbnail_id($post_id);
 
-                if ($post_image_id) {
-                    $post_listing_column_count = 'two';
-                    $post_image_size_name = 'medium-square';
-                    $post_image_url = wp_get_attachment_image_url($post_image_id, $post_image_size_name);
-                    $post_image_width = wp_get_attachment_image_src($post_image_id, $post_image_size_name)[1];
-                    $post_image_height = wp_get_attachment_image_src($post_image_id, $post_image_size_name)[2];
-                    $post_image_alt = get_post_meta($post_image_id, '_wp_attachment_image_alt', TRUE);
-                } else {
-                    $post_listing_column_count = 'one';
-                    $post_image_url = $post_image_width = $post_image_height = $post_image_alt = null;
-                }
+                    if ($post_image_id) {
+                        $post_listing_column_count = 'two';
+                        $post_image_size_name = 'medium-square';
+                        $post_image_url = wp_get_attachment_image_url($post_image_id, $post_image_size_name);
+                        $post_image_width = wp_get_attachment_image_src($post_image_id, $post_image_size_name)[1];
+                        $post_image_height = wp_get_attachment_image_src($post_image_id, $post_image_size_name)[2];
+                        $post_image_alt = get_post_meta($post_image_id, '_wp_attachment_image_alt', TRUE);
+                    } else {
+                        $post_listing_column_count = 'two';
+                        $post_image_url = $post_image_width = $post_image_height = $post_image_alt = null;
+                    }
 
-                $post_title = get_the_title($post_id);
-                $post_date = get_the_date('F j, Y', $post_id);
-                $post_date_datetime_format = get_the_date('Y-m-d', $post_id);
-                $post_author_id = get_post_field('post_author', $post_id);
-                $post_author_name = get_the_author_meta('display_name', $post_author_id);
-                $post_excerpt = get_the_excerpt($post_id) ?: null;
-            ?>
-                <article>
-                    <div class="content-row">
-                        <div class="columns">
+                    $post_title = get_the_title($post_id);
+                    $post_date = get_the_date('F j, Y', $post_id);
+                    $post_date_datetime_format = get_the_date('Y-m-d', $post_id);
+                    $post_author_id = get_post_field('post_author', $post_id);
+                    $post_author_name = get_the_author_meta('display_name', $post_author_id);
+                    $post_excerpt = get_the_excerpt($post_id) ?: null;
+                ?>
+                    <article>
+                        <div class="content-row">
                             <?php if ($post_image_id) : ?>
-                                <div
-                                    class="column left one-third"
-                                    data-mobile-hide="true">
-                                    <figure>
-                                        <a href="<?= get_the_permalink($post_id) ?>">
-                                            <img src="<?= esc_url($post_image_url) ?>" height="<?= esc_attr($post_image_height) ?>" width="<?= esc_attr($post_image_width) ?>" alt="<?= esc_attr($post_image_alt) ?>">
-                                        </a>
-                                    </figure>
-                                </div>
-                                <div class="column right two-thirds">
-                                    <h3><a href="<?= get_the_permalink($post_id) ?>"><?= esc_html($post_title) ?></a></h3>
-                                    <time datetime="<?= esc_attr($post_date_datetime_format) ?>"><?= esc_html($post_date) ?></time>
-                                    <span class="author">Author: <?= esc_html($post_author_name) ?></span>
-                                    <figure data-desktop-hide="true">
-                                        <a href="<?= get_the_permalink($post_id) ?>">
-                                            <img src="<?= esc_url($post_image_url) ?>" height="<?= esc_attr($post_image_height) ?>" width="<?= esc_attr($post_image_width) ?>" alt="<?= esc_attr($post_image_alt) ?>">
-                                        </a>
-                                    </figure>
-                                    <?php if ($post_excerpt) : ?>
-                                        <blockquote class="excerpt" cite="<?= esc_url(get_the_permalink($post_id)) ?>">
-                                            <?= wp_kses_post($post_excerpt) ?>
-                                        </blockquote>
-                                    <?php endif; ?>
-                                    <?php
-                                    echo get_template_part('views/conditional/posts/widgets/post-meta/post-category-list', null, array('id' => $post_id));
-                                    echo get_template_part('views/conditional/posts/widgets/post-meta/post-tag-list', null, array('id' => $post_id));
-                                    ?>
-                                    <a href="<?= get_the_permalink($post_id) ?>" class="button">Read Article</a>
-                                </div>
+                                <figure class="featured-post-image">
+                                    <a href="<?= get_the_permalink($post_id) ?>">
+                                        <img src="<?= esc_url($post_image_url) ?>" height="<?= esc_attr($post_image_height) ?>" width="<?= esc_attr($post_image_width) ?>" alt="<?= esc_attr($post_image_alt) ?>">
+                                    </a>
+                                </figure>
                             <?php else : ?>
-                                <div class="column">
-                                    <h3><a href="<?= get_the_permalink($post_id) ?>"><?= esc_html($post_title) ?></a></h3>
-                                    <time datetime="<?= esc_attr($post_date_datetime_format) ?>"><?= esc_html($post_date) ?></time>
-                                    <span class="author">Author: <?= esc_html($post_author_name) ?></span>
-                                    <?php if ($post_excerpt) : ?>
-                                        <blockquote class="excerpt" cite="<?= esc_url(get_the_permalink($post_id)) ?>">
-                                            <?= wp_kses_post($post_excerpt) ?>
-                                        </blockquote>
-                                    <?php endif; ?>
-                                    <?php
-                                    echo get_template_part('views/conditional/posts/widgets/post-meta/post-category-list', null, array('id' => $post_id));
-                                    echo get_template_part('views/conditional/posts/widgets/post-meta/post-tag-list', null, array('id' => $post_id));
-                                    ?>
-                                    <a href="<?= get_the_permalink($post_id) ?>" class="button">Read Article</a>
-                                </div>
+                                <figure class="featured-post-image featured-post-image--fallback">
+                                    <a href="<?= get_the_permalink($post_id) ?>">
+                                        <img src="/wp-content/themes/mrmastertheme/library/custom-theme/images/logos/logo.svg" alt="<?= esc_attr(get_bloginfo('name')) ?>">
+                                    </a>
+                                </figure>
                             <?php endif; ?>
+
+                            <div class="content">
+                                <h3><a href="<?= get_the_permalink($post_id) ?>"><?= esc_html($post_title) ?></a></h3>
+                                <p>Published On: <time datetime="<?= esc_attr($post_date_datetime_format) ?>"><?= esc_html(get_the_date('m-d-Y', $post_id)) ?></time></p>
+                                <?php
+                                echo get_template_part('views/conditional/posts/widgets/post-meta/post-category-list', null, array('id' => $post_id));
+                                ?>
+                            </div>
+                            <a href="<?= get_the_permalink($post_id) ?>" class="button">Read Article</a>
                         </div>
                         <span
                             class="row-settings"
@@ -95,9 +70,9 @@ if (
                             data-column-width="variable">
                             <span class="validator-text" data-nosnippet>row settings</span>
                         </span>
-                    </div>
-                </article>
-            <?php endforeach; ?>
+                    </article>
+                <?php endforeach; ?>
+            </div>
             <span
                 class="container-settings"
                 data-container-width="standard">

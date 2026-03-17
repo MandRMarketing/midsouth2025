4345752a9f8682bb6dbc210e7ef14f3ea4a8f3fd
Logan Scheid <scheid.logan@gmail.com>
2026-03-17 09:18:25 -0400

    Add favicon and web app metadata for improved branding
    - Included multiple favicon links and an apple touch icon to enhance the site's branding across different platforms.
    - Added a web app manifest and metadata for better integration with mobile devices, improving user experience and accessibility.

--- wp-content/themes/mrmastertheme/header.php
+++ wp-content/themes/mrmastertheme/header.php
@@ -7,6 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="dns-prefetch" href="https://www.googletagmanager.com">
 
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Midsouth Federal Credit Union" />
+    <link rel="manifest" href="/site.webmanifest" />
+
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
